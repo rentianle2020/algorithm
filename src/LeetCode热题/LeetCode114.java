f1c5fa559cf1829fa51f442c353b012e32c94c35
tyler <515322780@qq.com>
2021-11-20 18:38:03 +0800

    Hot100

---
+++ src/LeetCode热题/LeetCode114.java
@@ -0,0 +1,63 @@
+package LeetCode热题;
+
+import com.sun.source.tree.Tree;
+
+import java.util.LinkedList;
+
+public class LeetCode114 {
+
+    public void flatten(TreeNode root) {
+
+        if(root == null) return;
+
+        LinkedList<TreeNode> stack = new LinkedList<>();
+
+        stack.push(root);
+
+        TreeNode father = null;
+
+        while(!stack.isEmpty()){
+
+            TreeNode pop = stack.pop();
+
+            if(father != null){
+                father.right = pop;
+            }
+            father = pop;
+
+            if(father.right != null) stack.push(father.right);
+            if(father.left != null) stack.push(father.left);
+
+            father.left = null;
+        }
+    }
+
+
+    TreeNode parent = null;
+
+    public void flatten2(TreeNode root) {
+
+        if(root == null) return;
+        traversal(root);
+
+    }
+
+    private void traversal(TreeNode root) {
+
+        if(root == null) return;
+
+        if(parent != null){
+            parent.right = root;
+        }
+        parent = root;
+
+        TreeNode left = parent.left;
+        TreeNode right = parent.right;
+        parent.left = null;
+
+        traversal(left);
+        traversal(right);
+
+    }
+
+}
---
+++ src/LeetCode热题/LeetCode128.java
@@ -0,0 +1,36 @@
+package LeetCode热题;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 128. 最长连续序列
+ */
+public class LeetCode128 {
+
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int longestStreak = 0;
+
+        for (int num : set) {
+            if (!set.contains(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (set.contains(currentNum + 1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
+    }
+}
